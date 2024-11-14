2ccce2f12a58b9e99210b1118acdf997d73b522f
yeonjinKang <165971416+yeonjinnk@users.noreply.github.com>
2024-11-14 22:14:33 +0900

    [Bronze II] Title: 문자열 반복, Time: 92 ms, Memory: 14016 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -9,20 +9,16 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        //첫 줄 테스트 케이스 개수
+        //첫 줄 테스트 케이스 개수(반복문에 횟수로 사용 예정)
         int testCount = Integer.parseInt(br.readLine());
 
         //각 줄의 반복 횟수와 문자열을 공백으로 구분하기 위해
         StringTokenizer tokenizer;
 
-        //입력된 각 줄을 읽어와서 저장할 str 변수
-        String str = br.readLine();
-
-        //각 줄이 null을 만날 때까지 반복
+        //테스트 케이스 개수(testCount)만큼 반복
         for(int k = 0; k < testCount; k++) {
-//        while((str = br.readLine()) != null) {
-//            System.out.println(str);
-            tokenizer = new StringTokenizer(str, " "); //공백으로 구분
+            tokenizer = new StringTokenizer(br.readLine(), " "); //공백으로 구분자로 분리함
+
             int repeatNum = Integer.parseInt(tokenizer.nextToken()); //공백으로 구분된 token 처음이 반복횟수
             char[] tokenArr = tokenizer.nextToken().toCharArray(); //문자열로 된 token을 문자배열로 변환
 
@@ -32,9 +28,8 @@ public class Main {
                     bw.write(tokenArr[i]);
                 }
             } //각 줄 문자 반복하는 for문 끝
-            bw.write("\n");
-            bw.flush(); //출력문 버퍼에 저장
-            str = br.readLine();
+            bw.write("\n"); 
+            bw.flush(); //버퍼에 저장된 출력문 출력
         } //각 줄 읽어오는 for문 끝
         br.close(); //입력 스트림 닫음
         bw.close(); //출력 스트림 닫음
