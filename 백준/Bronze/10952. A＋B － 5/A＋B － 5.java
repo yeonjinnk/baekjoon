71aa722821365a46f2216aa5d4415dc3ed472341
yeonjinKang <165971416+yeonjinnk@users.noreply.github.com>
2024-11-29 19:23:04 +0900

    [Bronze V] Title: A+B - 5, Time: 184 ms, Memory: 17876 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class Main {
@@ -9,21 +10,21 @@ public class Main {
         [해결 과정] 우선순위 큐를 통해 최대 힙으로 카운트를 줄여가며 구한다
 
         [슈도 코드]
+        while 반복
         입력값 저장 변수 선언 a, b
         a+b 출력
-        a, b 모두 0보다 클 때 출력
+        a, b 모두 0일 때 break
         */
 
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        while(a > 0 && b > 0) {
+        while(true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(a == 0 & b == 0) {
+                break;
+            }
             System.out.println(a + b);
-            a = sc.nextInt();
-            b = sc.nextInt();
         }
-
-
 
     }
 }
