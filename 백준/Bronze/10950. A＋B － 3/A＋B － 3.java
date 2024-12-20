48fd4a81ffc4d50cf293b54a2d0334fc2816278a
yeonjinKang <165971416+yeonjinnk@users.noreply.github.com>
2024-12-20 18:01:58 +0900

    [Bronze V] Title: A+B - 3, Time: 188 ms, Memory: 17888 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i = 0; i < T; i++) {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            System.out.println(n1 + n2);
+        }
+    }
+}
